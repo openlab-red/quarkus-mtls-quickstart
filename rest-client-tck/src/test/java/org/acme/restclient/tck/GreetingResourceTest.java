d05133ef2a2d3f8ee6fda6936e65b046de4c3ade
Mattia Mascia <mmascia@redhat.com>
2020-03-04 08:12:25 +0100

    clean up

--- rest-client-tck/src/test/java/org/acme/restclient/tck/GreetingResourceTest.java
+++ rest-client-tck/src/test/java/org/acme/restclient/tck/GreetingResourceTest.java
@@ -5,7 +5,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.Ignore;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
