3e7fac216b0c048d9f22ef0bef89c081b9c5ad3b
Mattia Mascia <mmascia@redhat.com>
2020-03-04 08:08:56 +0100

    diagram

---
+++ images/diagram.py
@@ -0,0 +1,30 @@
+# diagram.py
+from urllib.request import urlretrieve
+
+from diagrams import Cluster, Diagram
+from diagrams.custom import Custom
+from diagrams.k8s.network import Ingress, Service
+from diagrams.k8s.compute import Pod
+from diagrams.k8s.rbac import User
+
+
+quarkus_url = "https://www.kindpng.com/picc/m/275-2755570_quarkus-png-transparent-png.png"
+quarkus_icon = "quarkus.png"
+
+urlretrieve(quarkus_url, quarkus_icon)
+
+with Diagram("Mutual TLS", show=False):
+
+    with Cluster("Request"):
+        users = [User("Users")]
+
+    with Cluster("Mutual TLS"):
+        client = Custom("http://client:8080/hello", quarkus_icon)
+        server = Custom("https://server:8443/hello", quarkus_icon)
+        client >> [server]
+        server >> [client]
+
+    users >> client
+
+
+    
